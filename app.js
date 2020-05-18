eb0d304379d66f4b9121af48428907696815db36
鄭又誠 <zxcy9980@gmail.com>
2020-05-18 17:45:58 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -2,27 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 const exphbs = require('express-handlebars')
-const mongoose = require('mongoose') // 載入 mongoose
-const Todo = require('./models/todo') // 載入 Todo model
 const bodyParser = require('body-parser')
 // 引用路由器
 const routes = require('./routes')
 
+
 // 載入 method-override
 const methodOverride = require('method-override')
-// 設定連線到 mongoDB
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true }) 
 
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,12 +1,7 @@
-const mongoose = require('mongoose')
-const Todo = require('../todo') // 載入 todo model
-mongoose.connect('mongodb://localhost/restaurant_list', { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
+const db = require('../../config/mongoose')
+const Todo = require('../../models/todo')
+
+db.once('open', () => {  
   Todo.create(     
       {
         "id": 1,
